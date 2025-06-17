c7cd425478cc571c5d6441645be8d948a5dbb95c
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-06-17 21:56:54 +0900

    구성 변경 이후에도 상태가 보존되는 Saveable 변수 확인

--- CleanArchitectureMovieApp/app/src/main/java/com/example/cleanarchitecturemovieapp/presentation/activity/MovieActivity.kt
+++ CleanArchitectureMovieApp/app/src/main/java/com/example/cleanarchitecturemovieapp/presentation/activity/MovieActivity.kt
@@ -20,6 +20,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -56,6 +60,11 @@ class MovieActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun SaveableTest() {
+    var count by rememberSaveable { mutableIntStateOf(0) }
 }
 
 @Composable
